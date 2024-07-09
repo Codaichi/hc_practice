3bd478cf67dccecb5c45ccd2181420146c06730e
codaichi <d.hirabayashi89@gmail.com>
2024-07-09 22:01:44 +0900

    update the code for group splitting part

--- python/randam.py
+++ python/randam.py
@@ -7,14 +7,10 @@ def split_group(m):
     # shuffle members
     random.shuffle(m)
     # split group 3 by 3 or 2 by 4
-    if random.choice([0, 1]) == 0:
-        first_group = m[:3]
-        second_group = m[3:]
-        return first_group, second_group
-    else:
-        first_group = m[:2]
-        second_group = m[2:]
-        return first_group, second_group
+    num = random.choice([2, 3])
+    first_group = m[:num]
+    second_group = m[num:]
+    return first_group, second_group
 
 first_group, second_group = split_group(m)
 first_group.sort()
